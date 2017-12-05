d8693b0e6f8e780b18a4f130535efba7684e148c
Arcs96 <arcdj.96@gmail.com>
2017-12-05 20:29:35 +0100

    fichero aprovisionamiento

--- act3b/config.sh
+++ act3b/config.sh
@@ -1,2 +1,8 @@
 sudo apt-get -y update
 sudo apt-get -y install apache2
+sudo apt-get -y install git
+cd /var/www
+sudo rm -rf html
+cd /vagrant
+git clone https://github.com/boputu/adventuresport.git
+sudo ln -fs /vagrant/adventuresport /var/www/html
